dd6a93a1278b9afc09f3528de2137d02da490646
Laíse Vieira <laisevdev@gmail.com>
2023-07-18 16:14:02 -0300

    [feat]:add func connectWallet

--- nftMint-app/src/components/ConnectButton/ConnectButton.jsx
+++ nftMint-app/src/components/ConnectButton/ConnectButton.jsx
@@ -1,9 +1,27 @@
 import './ConnectButton.css'
+import { ethers } from 'ethers'
+import { useState } from 'react';
+
 
 const ConnectButton = () => {
+
+  const [message, setMessage] = useState("");
+
+  async function connecWallet(){
+    setMessage("")    
+    if(!window.ethereum) return setMessage("Carteira não encontrada");
+
+    const provider = new ethers.providers.web3Provider(window.ethereum);
+    const accounts = await provider.send("eth_requestAccounts", []);
+    if(!accounts || !accounts.length) return setMessage("Carteira não autorizada")
+
+  }
+
   return (
     <div> 
-        <button type='button'>Conectar Carteira</button> 
+        <button type='button' onClick={connecWallet}>Conectar Carteira</button> 
+        <br/>
+        <p>{message}</p>
     </div>
   
   )
