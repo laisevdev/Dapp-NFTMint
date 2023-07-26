35371d57659e8a1ec122c0686b9b9ea52c055c27
Laíse Vieira <laisevdev@gmail.com>
2023-07-25 22:38:57 -0300

    [refactor]: func mint

--- eth-contracts/contracts/nftMint.sol
+++ eth-contracts/contracts/nftMint.sol
@@ -3,17 +3,20 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 
-contract MyNFT is ERC721 {
+contract MyNFT is ERC721URIStorage {
     uint256 public id;
     address public owner;
     uint256 priceTotal;
     uint256 public supplyNFT= 4;
     uint256 public price = 0.001 ether;
-    string baseUri = "https://ipfs.io/ipfs/Qmc2NJLkBumcL1G4VrYKn6b1rFm1EVvA7RHsh1TdqZTfXG/";
 
-    using Strings for uint256;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+    
 
     modifier OnlyOwner () {
         require(msg.sender == owner);
@@ -24,29 +27,24 @@ contract MyNFT is ERC721 {
         owner = msg.sender;
     }
 
-    function mint(uint256 amount) public payable {
+    function mint(uint256 amount, string memory uri) public payable {
+        
         priceTotal = amount * price;
+        require(amount + id <= supplyNFT, "Sold out" );
         require(msg.value == priceTotal, "Enough Balance");
 
         for(uint256 i =0; i < amount; i++) {
-            id += 1;
-            _safeMint(msg.sender, id);
-        }
+            id +=1;
+            uint256 tokenId = _tokenIdCounter.current();
+            _tokenIdCounter.increment(); 
+
+            _safeMint(msg.sender, tokenId);         
+            _setTokenURI(tokenId, uri);       
+        }        
     }
 
     function supplyTotal() public view returns (uint256) {
         return id;
-    }
-
-    function uri() internal view virtual returns (string memory) {
-        return baseUri;
-    }
-
-    function tokenURI(uint256 _id) public view virtual override returns (string memory) {
-        string memory baseURI = uri();
-        string memory json = ".json";
-        
-        return bytes(baseURI).length > 0 ? string.concat(baseURI, _id.toString(), json) : "Error";
     }
 
     function getBalance() public view returns (uint256) {
