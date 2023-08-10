06c704544d93a4213757afe707b207444893984f
Laíse Vieira <laisevdev@gmail.com>
2023-08-10 13:39:27 -0300

    [refactor]: import image logotipo

--- nftMint-app/src/components/Logotipo/Logotipo.jsx
+++ nftMint-app/src/components/Logotipo/Logotipo.jsx
@@ -1,8 +1,12 @@
 import './Logotipo.css'
+import imglogo from '../../imgs/foto-logo.png'
+
+
+const logo = imglogo;
 
 const Logotipo = () => {
 
-  return <img className='image' src='../src/imgs/foto-logo.png' alt='Logotipo' />;
+  return <img className='image' src={logo} alt='Logotipo' />;
 }
 
 export default Logotipo
